67fad8f728da9ef8837fbef530e7b28f98c7e0cf
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-08-03 18:02:58 -0400

    created arrays and functions to calculate total starting and current value for the portfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,26 @@
-//Alpha Vantage API Key: 0V05X9O48C7R2P6N
-var savedStockList;
-var stockList = [];
-
-//
-// TO DO: Retrieve the stocks from local Storage and store in stockList array
-//
-
-/**
- * @type {JSON} savedStockList - Get stocks detail container from previous save.
- */
-savedStockList = JSON.parse(localStorage.getItem("stocks"));
-
-// Display saved list of stocks
-for (let i = 0; i < savedStockList.length; i++) {
-    getSymbol(savedStockList[i]);
-}
-
-
-function save(savedStockList)
-    for (let i = 0; i < savedStockList.length; i++) {
-        let stock = savedStockList[i];
-        getSymbol(stock);
-    }
-
-function createCard(response) {
-    // Create a new boostrap card container
-    let article = $("<article>");
-    article.addClass("card");
-
-      // Create a new card body container
-      var cardBody = $("<div>");
-      cardBody.addClass("card-body");
-
-      // Add stock name
-      var stockName = $("<h1>");
-      stockName.addClass("card-title");
-      console.log(response)
-      stockName.html(response["Meta Data"]['2. Symbol']);
-      article.append(stockName);
-
-      // Add information/ metrics
-      var series= response["Time Series (Daily)"];
-      var dailyOpen = $("<p>");
-      dailyOpen.addClass("card-text");
-      dailyOpen.html(response["Time Series (Daily)"]["2019-07-30"]["1. open"]);
-      article.append(dailyOpen);
-
-      var dailyHigh = $("<p>");
-      dailyHigh.addClass("card-text");
-      dailyHigh.html(response["Time Series (Daily)"]["2019-07-30"]["2. high"]);
-      article.append(dailyHigh);
-
-      var dailyLow = $("<p>");
-      dailyLow.addClass("card-text");
-      dailyLow.html(response["Time Series (Daily)"]["2019-07-30"]["3. low"]);
-      article.append(dailyLow);
-
-      var dailyClose = $("<p>");
-      dailyClose.addClass("card-text");
-      dailyClose.html(response["Time Series (Daily)"]["2019-07-30"]["4. close"]);
-      article.append(dailyClose);
-
-
-    // Append the new card to the HTML body
-    $("#finance-section").append(article);
-
-}
-
-/**
- * getSymbol() get the stock symbol via Alpha Vantage API and add it to stockList.
- * @param {string} stockSymbol - User's input in the search bar.
- */
 function getSymbol(stockSymbol) {
 
 
-      // queryURL endpoint for Alpha Vantage API
-      var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+stockSymbol+"&apikey=0V05X9O48C7R2P6N";
-console.log(queryURL)
-      // AJAX call to Alpha Vantage API with promise and callback handler
-      $.ajax({
-
+    // queryURL endpoint for Alpha Vantage (Daily)API
+    var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stockSymbol + "&apikey=0V05X9O48C7R2P6N";
+    console.log(queryURL)
+    // AJAX call to Alpha Vantage API with promise and callback handler
+    $.ajax({
         url: queryURL,
         method: "GET"
-    }).done(function(response) {
+    }).done(function (response) {
         console.log("hello");
-        if (response.Response === "False") {
-            alert(response.Error);
-        }
-        else if (stockList.indexOf(response.symbol) >= 0) {
-            alert ("Stock already in List!")
-        }
-        else 
-        {
-            createCard(response);
-            stockList.push(response.symbol);
-
-            //
-            // TO DO: Save the stocks in the stockList array to Local Storage
-            //
-          
-            localStorage.setItem("stocks", JSON.stringify(stockList));
-
-        }
+        $.ajax({
+            url:"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" + stockSymbol +"&apikey=0V05X9O48C7R2P6N",
+            method: "GET"
+        }).done(function (response2){
+            console.log("hello");
+            createCard(response, response2);
+        });
+        // createCard(response);
     });
 
-}
-
-// Create a bootstrap card for each item in the stockList array
-
-for (var i = 0; i < stockList.length; i++) {
-
-    var stock = stockList[i];
-    getSymbol(stock);
-      
+    
 }
 
 //
@@ -120,9 +28,107 @@ for (var i = 0; i < stockList.length; i++) {
 // and call the AJAX function, passing the name of the new stock
 //
 
-$("#search").click(function() {
-    console.log("hello");
-    var stock = $("#stock-name").val();
-    getSymbol(stock);
+var v1= [];
+
+var v2= [];
+
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+      ]
+    }
+});
+$("#search").click(function () {
+    var symbol = $("#inputSymbol").val();
+    getSymbol(symbol);
+
+
+});
+
+//create a card with different metrics//
+
+function createCard(response, response2) {
+    //create today's date and format it to the same date as the API//
+    // Create a new boostrap card container
+    var article = $("<article>");
+    article.addClass("card");
+    article.addClass("stockCard")
+
+    // Create a new card body container
+    var cardBody = $("<div>");
+    cardBody.addClass("card-body");
+
+    // Add stock name
+    var stockName = $("<h1>");
+    stockName.addClass("card-title");
+    console.log(response)
+    stockName.html(response["Meta Data"]['2. Symbol']);
+    article.append(stockName);
+
+    // Add information/ metrics
+    //date (daily) function//
+    var date = response["Meta Data"]["3. Last Refreshed"];
+    console.log(date)
+    var todayDate = $("<p>");
+    todayDate.addClass("card-text");
+    todayDate.html(response["Meta Data"]["3. Last Refreshed"]);
+    article.append(todayDate);
+//end date (daily) function//
+
+//date function (monthly)//
+    var monthDate = response2["Meta Data"]["3. Last Refreshed"];
+    
+    
+    var series = response2["Monthly Time Series"];
+    var monthOpen = $("<p>");
+    monthOpen.addClass("card-text");
+    monthOpen.html(response2["Monthly Time Series"][monthDate]["1. open"]);
+    article.append("Starting Value (Monthly Open): ", monthOpen);
+    var startValue= response2["Monthly Time Series"][monthDate]["1. open"];
+    v1.push(response2["Monthly Time Series"][monthDate]["1. open"]);
+
+    var dailyClose = $("<p>");
+    dailyClose.addClass("card-text");
+    dailyClose.html(response["Time Series (Daily)"][date]["4. close"]);
+    article.append("Current Value (Daily Close): ", dailyClose);
+    var currentValue= response["Time Series (Daily)"][date]["4. close"];
+    v2.push(response["Time Series (Daily)"][date]["4. close"]);
+
+    var monthHigh = $("<p>");
+    monthHigh.addClass("card-text");
+    monthHigh.html(response2["Monthly Time Series"][monthDate]["2. high"]);
+    article.append("Monthly High: ", monthHigh);
+
+    var monthLow = $("<p>");
+    monthLow.addClass("card-text");
+    monthLow.html(response2["Monthly Time Series"][monthDate]["3. low"]);
+    article.append("Monthly Low: ", monthLow);
+
+    // Append the new card to the HTML body
+    $("#stockData").append(article);
+
+    // var chart = c3.generate({
+    //     bindto: '#chart',
+    //     data: {
+    //       columns: [
+    //         [stockName, startValue, currentValue],
+            
+    //       ]
+    //     }
+    // });
+    chart.load({
+        columns: [
+            [response["Meta Data"]['2. Symbol'], startValue, currentValue]
+        ]
     });
 
+    function sum(total, num) {
+        return parseFloat(total) + parseFloat(num);
+    }
+    
+    $("#startingValue").text(v1.reduce(sum));
+    $("#currentValue").text(v2.reduce(sum));
+
+}

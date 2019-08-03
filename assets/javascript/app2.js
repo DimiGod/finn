b10a6716246aa846036118f318a398c04fea1bc1
Sitthiph <47881002+sitthiph@users.noreply.github.com>
2019-08-03 14:34:02 -0400

    changes indentation

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,69 +1,87 @@
 function getSymbol(stockSymbol) {
 
-    // queryURL endpoint for Alpha Vantage API
-    var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+stockSymbol+"&apikey=0V05X9O48C7R2P6N";
-console.log(queryURL)
-    // AJAX call to Alpha Vantage API with promise and callback handler
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-     console.log("hello"); 
-    });
+  // queryURL endpoint for Alpha Vantage API
+  var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+stockSymbol+"&apikey=0V05X9O48C7R2P6N";
+  console.log(queryURL);
+  // AJAX call to Alpha Vantage API with promise and callback handler
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log("hello");
     createCard(response);
-  }
-  
-  //
-  // Create button click handler to get the form submission,
-  // and call the AJAX function, passing the name of the new stock
-  //
-  $("#search").click(function() {
-    var symbol = $("#inputSymbol").val();
-    getSymbol(symbol);
-    
-//create function for the current date in the same format as the API
   });
 
-  //create a card with different metrics//
-  function createCard(response) {
-    // Create a new boostrap card container
-    var article = $("<article>");
-    article.addClass("card");
+}
 
-    // Create a new card body container
-    var cardBody = $("<div>");
-    cardBody.addClass("card-body");
+//
+// Create button click handler to get the form submission,
+// and call the AJAX function, passing the name of the new stock
+//
 
-    // Add stock name
-    var stockName = $("<h1>");
-    stockName.addClass("card-title");
-    console.log(response)
-    stockName.html(response["Meta Data"]['2. Symbol']);
-    article.append(stockName);
 
-    // Add information/ metrics
-    var series= response["Time Series (Daily)"];
-    var dailyOpen = $("<p>");
-    dailyOpen.addClass("card-text");
-    dailyOpen.html(response["Time Series (Daily)"]["2019-07-30"]["1. open"]);
-    article.append(dailyOpen);
 
-    var dailyHigh = $("<p>");
-    dailyHigh.addClass("card-text");
-    dailyHigh.html(response["Time Series (Daily)"]["2019-07-30"]["2. high"]);
-    article.append(dailyHigh);
+$("#search").click(function() {
+  var symbol = $("#inputSymbol").val();
+  getSymbol(symbol);
 
-    var dailyLow = $("<p>");
-    dailyLow.addClass("card-text");
-    dailyLow.html(response["Time Series (Daily)"]["2019-07-30"]["3. low"]);
-    article.append(dailyLow);
 
-    var dailyClose = $("<p>");
-    dailyClose.addClass("card-text");
-    dailyClose.html(response["Time Series (Daily)"]["2019-07-30"]["4. close"]);
-    article.append(dailyClose);
+});
 
-    // Append the new card to the HTML body
-    $("#stockData").append(article);
+//create a card with different metrics//
+function createCard(response) {
+//create today's date and format it to the same date as the API//
+  // Create a new boostrap card container
+  var article = $("<article>");
+  article.addClass("card");
 
+  // Create a new card body container
+  var cardBody = $("<div>");
+  cardBody.addClass("card-body");
+
+  // Add stock name
+  var stockName = $("<h1>");
+  stockName.addClass("card-title");
+  console.log(response)
+  stockName.html(response["Meta Data"]['2. Symbol']);
+  article.append(stockName);
+
+  // Add information/ metrics
+  function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
+  var today = formatDate();
+  console.log(today);
+  var series= response["Time Series (Daily)"];
+  var dailyOpen = $("<p>");
+  dailyOpen.addClass("card-text");
+  dailyOpen.html(response["Time Series (Daily)"][today]["1. open"]);
+  article.append("Open: ", dailyOpen);
+
+  var dailyHigh = $("<p>");
+  dailyHigh.addClass("card-text");
+  dailyHigh.html(response["Time Series (Daily)"][today]["2. high"]);
+  article.append("Daily High: ", dailyHigh);
+
+  var dailyLow = $("<p>");
+  dailyLow.addClass("card-text");
+  dailyLow.html(response["Time Series (Daily)"][today]["3. low"]);
+  article.append("Daily Low: ", dailyLow);
+
+  var dailyClose = $("<p>");
+  dailyClose.addClass("card-text");
+  dailyClose.html(response["Time Series (Daily)"][today]["4. close"]);
+  article.append("Close: ",dailyClose);
+
+  // Append the new card to the HTML body
+  $("#stockData").append(article);
+
+}
